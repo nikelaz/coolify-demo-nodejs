4d943aa8ca455cfcdd2dd8986eadb9c92be53a05
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2024-06-08 20:59:09 +0300

    update port variable

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ fastify.get('/', function handler (request, reply) {
   reply.send({ hello: 'world' })
 });
 
-fastify.listen({ port: process.env.PORT, host: '0.0.0.0' }, (err) => {
+fastify.listen({ port: 3000, host: '0.0.0.0' }, (err) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
