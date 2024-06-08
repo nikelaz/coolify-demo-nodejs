f5b3bc90fffe7c22bce1dd150dac1f6fa43722e7
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2024-06-08 20:56:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ fastify.get('/', function handler (request, reply) {
   reply.send({ hello: 'world' })
 });
 
-fastify.listen({ port: process.env.PORT }, (err) => {
+fastify.listen({ port: process.env.PORT, host: '0.0.0.0' }, (err) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
