9f2e44c439aefff3812a0eeeeba8e44674b29bfe
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2024-06-08 18:21:41 +0300

    change port env variable

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ fastify.get('/', function handler (request, reply) {
   reply.send({ hello: 'world' })
 });
 
-fastify.listen({ port: process.env.port }, (err) => {
+fastify.listen({ port: process.env.PORT }, (err) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
